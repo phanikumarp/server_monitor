8963df9865edfd1e87b7823198a2b29ba1aabd90
Basivireddy <basivireddy@users.noreply.github.com>
2016-03-16 23:21:57 +0530

    Create Agent.py

---
+++ Agent.py
@@ -0,0 +1,15 @@
+import time
+import subprocess
+
+checks = ["python /opt/agents/n42/SystemCheck.py","python /opt/agents/n42/NetworkCheck.py"]
+
+if __name__ == "__main__":
+     i = 4;	
+     while True:
+	procs = [subprocess.Popen(check, shell=True) for check in checks]		       	
+	#for check in checks:
+	#       #http://stackoverflow.com/a/19393328/745018
+	#       procs = [subprocess.Popen(['svn', 'update', repo]) for repo in repos]		       	
+        print "created subprocess" 
+        #i = i-1
+        time.sleep(4)
