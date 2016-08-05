2d0ddcb5d416ca5b5f9d525381ff78c33aad1e45
OpsMx <accounts@opsmx.com>
2016-08-05 16:09:52 +0530

    Update packetbeat_install.sh

--- packetbeat_install.sh
+++ packetbeat_install.sh
@@ -2,7 +2,7 @@
 sudo apt-get install -y libpcap0.8
 curl -L -O https://download.elastic.co/beats/packetbeat/packetbeat_1.2.2_amd64.deb
 sudo dpkg -i packetbeat_1.2.2_amd64.deb
-sudo wget -q -O /etc/packetbeat/packetbeat.yml https://raw.githubusercontent.com/basivireddy/server_monitor/master/packetbeat.yml
+sudo wget -q -O /etc/packetbeat/packetbeat.yml https://raw.githubusercontent.com/N42Inc/server_monitor/master/packetbeat.yml
 i=`curl http://169.254.169.254/latest/meta-data/ami-id`
 sed -i "s/shipper_name/$i/g" /etc/packetbeat/packetbeat.yml
-sudo wget -q -O /etc/rc.local https://raw.githubusercontent.com/basivireddy/server_monitor/master/rc.local
+sudo wget -q -O /etc/rc.local https://raw.githubusercontent.com/N42Inc/server_monitor/master/rc.local
