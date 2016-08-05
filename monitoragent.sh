6c7b17db3f5509c40bf03ea3cc0178fd48789ed1
OpsMx <accounts@opsmx.com>
2016-08-05 16:11:06 +0530

    Update monitoragent.sh

--- monitoragent.sh
+++ monitoragent.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
 ### BEGIN INIT INFO
-# Provides:          n42Agent
+# Provides:          n42agent
 # Required-Start:    $local_fs $network $named $time $syslog
 # Required-Stop:     $local_fs $network $named $time $syslog
 # Default-Start:     2 3 4 5
 # Default-Stop:      0 1 6
-# Description:       n42Agent
+# Description:       n42agent
 ### END INIT INFO
+
+#Follow standards https://wiki.debian.org/LSBInitScripts
 
 SCRIPT="stdbuf -oL nohup python /opt/agents/monitor/Agent.py"
 #RUNAS=<USERNAME>
 
-NAME="monitorAgent"
+NAME="monitoragent"
 PIDFILE="/var/run/$NAME.pid"
 LOGFILE="/var/log/$NAME.log"
 
@@ -47,6 +49,15 @@ stop() {
   fi  
 }
 
+status() {
+  if [ -f "$PIDFILE" ] && (ps -p $(cat "$PIDFILE") > /dev/null);
+  then
+    echo "Service is running" >&2
+  else
+    echo 'Service is not running' >&2
+  fi  
+}
+
 uninstall() {
   echo -n "Are you really sure you want to uninstall this service? That cannot be undone. [yes|No] "
   local SURE
@@ -54,9 +65,9 @@ uninstall() {
   if [ "$SURE" = "yes" ]; then
     stop
     rm -f "$PIDFILE"
-    echo "Notice: log file is not be removed: '$LOGFILE'" >&2
-    #update-rc.d -f <NAME> remove
-    #rm -fv "$0"
+    echo "Notice: log file is not to be removed: '$LOGFILE'" >&2
+    update-rc.d -f $NAME remove
+    rm -fv "$0"
   fi
 }
 
@@ -70,10 +81,13 @@ case "$1" in
   uninstall)
     uninstall
     ;;
+  status)
+    status
+    ;;  
   restart)
     stop
     start
     ;;
   *)
-    echo "Usage: $0 {start|stop|restart|uninstall}"
+    echo "Usage: $0 {start|stop|restart|status|uninstall}"
 esac
